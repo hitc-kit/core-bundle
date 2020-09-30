04cfe6938df37fcdce9860cfd7692fedac59cad4
Maksim Voskresenskii <17379328+MaksFM@users.noreply.github.com>
2020-09-30 21:41:50 +0700

    Добавлена конфигурация через DependencyInjection

---
+++ DependencyInjection/HITcKitCoreExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace HITcKit\CoreBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class HITcKitCoreExtension extends Extension
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator('@HITcKitCoreBundle/Resources/config')
+        );
+        $loader->load('common.yaml');
+    }
+}

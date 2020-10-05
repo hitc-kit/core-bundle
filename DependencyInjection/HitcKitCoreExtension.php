8d912a7e6cc019e45e4893a823913bcbfad13e7d
Maksim Voskresenskii <17379328+MaksFM@users.noreply.github.com>
2020-10-05 13:17:25 +0700

    Новая зависимость - "knplabs/knp-menu-bundle"

--- DependencyInjection/HitcKitCoreExtension.php
+++ DependencyInjection/HitcKitCoreExtension.php
@@ -2,77 +2,83 @@
 
 namespace HitcKit\CoreBundle\DependencyInjection;
 
-use App\Controller\DefaultController;
-use App\Entity\Product;
-use HitcKit\CoreBundle\Entity\RouteOrm;
+use Exception;
+// use HitcKit\CoreBundle\Entity\RouteOrm;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class HitcKitCoreExtension extends Extension implements PrependExtensionInterface
 {
-
     /**
      * @inheritDoc
+     * @throws Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        // $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        // $loader->load('common.yaml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 
     public function prepend(ContainerBuilder $container)
     {
         $bundles = $container->getParameter('kernel.bundles');
 
-        if (!isset($bundles['CmfRoutingBundle'])) {
-            return;
-        }
-
-        $config = [
-            'chain' => [
-                'routers_by_id' => [
-                    'router.default' => 200,
-                    'cmf_routing.dynamic_router' => 100,
-                ],
-            ],
-            'dynamic' => [
-                'persistence' => [
-                    'orm' => [
-                        'enabled' => true,
-                        'route_class' => RouteOrm::class,
+        if (isset($bundles['CmfRoutingBundle'])) {
+            $config = [
+                'chain' => [
+                    'routers_by_id' => [
+                        'router.default' => 200,
+                        'cmf_routing.dynamic_router' => 100,
                     ],
                 ],
-                'controllers_by_class' => [
-                    Product::class => DefaultController::class.'::index'
-                ],
-            ],
-        ];
-
-        $container->prependExtensionConfig('cmf_routing', $config);
-
-        if (!isset($bundles['DoctrineBundle'])) {
-            return;
-        }
-
-        $config = [
-            'orm' => [
-                'entity_managers' => [
-                    'default' => [
-                        'mappings' => [
-                            'HitcKitCoreBundle' => [
-                                'type' => 'xml',
-                                'dir' => 'Resources/config/doctrine-orm',
-                                'prefix' => 'HitcKit\CoreBundle\Entity',
-                                'alias' => 'hitc_kit_core',
-                                'is_bundle' => true,
-                            ],
+                'dynamic' => [
+                    'persistence' => [
+                        'orm' => [
+                            'enabled' => true,
+                            // 'route_class' => RouteOrm::class,
                         ],
                     ],
                 ],
-            ],
-        ];
+            ];
 
-        $container->prependExtensionConfig('doctrine', $config);
+            $container->prependExtensionConfig('cmf_routing', $config);
+        }
+
+        if (isset($bundles['StofDoctrineExtensionsBundle'])) {
+            $config = [
+                'orm' => [
+                    'default' => [
+                        'tree' => true,
+                    ],
+                ],
+            ];
+
+            $container->prependExtensionConfig('stof_doctrine_extensions', $config);
+
+            if (isset($bundles['DoctrineBundle'])) {
+                $config = [
+                    'orm' => [
+                        'entity_managers' => [
+                            'default' => [
+                                'mappings' => [
+                                    'gedmo_tree' => [
+                                        'type' => 'annotation',
+                                        'dir' => '%kernel.project_dir%/vendor/gedmo/doctrine-extensions/src/Tree/Entity',
+                                        'prefix' => 'Gedmo\\Tree\\Entity',
+                                        'alias' => 'GedmoTree',
+                                        'is_bundle' => false,
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ];
+
+                $container->prependExtensionConfig('doctrine', $config);
+            }
+        }
     }
 }
